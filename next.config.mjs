7d0a55f5d5a860a5a0f5fb703fb183c10f4323a0
= <info@farunurisonmez.com>
2024-05-21 21:23:42 +0300

    #9
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,4 @@
-const path = require("path");
-const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const withPlugins = require("next-compose-plugins");
-const MDX = require("@next/mdx");
-
-// next.js configuration
-const nextConfig = {
-  webpack5: false,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-    config.resolve.alias.images = path.join(__dirname, "images");
-    return config;
-  },
-  pageExtensions: ["js", "jsx", "md", "mdx"],
-  target: "serverless",
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-    };
-  },
-  basePath: ghPages ? "/farunurisonmez.github.io/" : "",
-  assetPrefix: ghPages ? "/farunurisonmez.github.io/" : "",
-};
-
-module.exports = withPlugins(
-  [
-    [
-      MDX,
-      {
-        extension: /\.mdx?$/,
-      },
-    ],
-  ],
-  nextConfig
-);
+export default nextConfig;
--- src/app/mdx-page/page.tsx
+++
@@ -1,5 +0,0 @@
-import Welcome from '@/app/(routes)/(blogs)/blog1.mdx'
- 
-export default function Page() {
-  return <Welcome />
-}
